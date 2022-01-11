33bb0f642bcc53dc3cf86ca644f579879ef3ba60
Salvador Lopez <salvatore.isc@gmail.com>
2022-01-11 09:35:46 -0600

    Omitir el numero de secciones por defecto

--- practica2/ViewController.swift
+++ practica2/ViewController.swift
@@ -25,10 +25,6 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate{
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contactos.count
     }
